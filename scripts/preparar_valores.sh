045c517bc36252a657a9c0ac051c838705b2c405
EzequielPuerta <eze19.2009@gmail.com>
2019-12-09 16:22:57 +0000

    Actualizarscripts/preparar_valores.sh

--- scripts/preparar_valores.sh
+++ scripts/preparar_valores.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 EXPERIMENT_PATH=$1
-PHARO_PATH="/home/ezequiel/PharoLauncher/pharo-vm/"
-FIRST_SCRIPT="/home/ezequiel/GIT/SimulacionDeEventosDiscretos/TP2/scripts/subdividir_logs.st"
-SECOND_SCRIPT="/home/ezequiel/GIT/SimulacionDeEventosDiscretos/TP2/scripts/extraer_valores.st"
+PHARO_PATH="../../PharoLauncher/pharo-vm/"
+FIRST_SCRIPT="../../TP2/scripts/subdividir_logs.st"
+SECOND_SCRIPT="../../TP2/scripts/extraer_valores.st"
 
 cd $PHARO_PATH
 ./pharo Pharo.image eval --save Smalltalk at: \'fullPath\' asSymbol put: $EXPERIMENT_PATH,\'/log/\'.
