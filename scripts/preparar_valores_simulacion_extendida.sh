9db5e3806cf852736884c896f6c465cb5914a839
EzequielPuerta <eze19.2009@gmail.com>
2019-12-09 16:24:28 +0000

    Actualizarscripts/preparar_valores_simulacion_extendida.sh

--- scripts/preparar_valores_simulacion_extendida.sh
+++ scripts/preparar_valores_simulacion_extendida.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 EXPERIMENT_PATH=$1
-PHARO_PATH="/home/ezequiel/PharoLauncher/pharo-vm/"
-SCRIPT="/home/ezequiel/GIT/SimulacionDeEventosDiscretos/TP2/scripts/subdividir_presupuesto.st"
+PHARO_PATH="../../PharoLauncher/pharo-vm/"
+SCRIPT="../../TP2/scripts/subdividir_presupuesto.st"
 
 cd $PHARO_PATH
 ./pharo Pharo.image eval --save Smalltalk at: \'fullPath\' asSymbol put: $EXPERIMENT_PATH,\'/log/\'.
